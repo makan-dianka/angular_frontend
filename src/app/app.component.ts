7fbe5b0eb0be783066029f83a752d46beeb93169
Makan DIANKA <makan.dianka@hotmail.com>
2023-01-05 09:09:03 +0100

    observable subscribe and unsubscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
 import { AppareilService } from './services/appareil.service';
 
 @Component({
@@ -6,6 +7,22 @@ import { AppareilService } from './services/appareil.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
+export class AppComponent implements OnInit, OnDestroy {
+
   constructor(){}
+  secondes: number | undefined;
+  counterSubscription: Subscription | undefined
+
+  ngOnInit(): void {
+    const counter = interval(1000)
+    this.counterSubscription = counter.subscribe(
+      (value: number) => {
+        this.secondes = value
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.counterSubscription?.unsubscribe();
+  }
 }
